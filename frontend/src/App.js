7e117c1dcbd2960d98f537130bc91f13ed1f8ec1
Usman <u2003252@gmail.com>
2024-03-27 23:03:43 +0500

    App.js updated i.e. Website Structure Updated and Improved Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -47,13 +47,60 @@
 
 
 
+// import React, { useState } from 'react';
+// import "./App.css";
+// import Home from "./Pages/Home/Home";
+// import Sidebar from "./Components/Sidebar/Sidebar";
+// import Messages from "./Pages/Messages/Messages";
+// import Search from "./Pages/Search/Search";
+// import Create from "./Pages/Create/Create";
+// import Notifications from "./Pages/Notifications/Notifications";
+// import Profile from "./Pages/Profile/Profile";
+// import Login from "./Pages/Login/Login";
+// import Signup from "./Pages/Signup/Signup";
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import EditProfile from './Pages/EditProfile/EditProfile';
+
+// function App() {
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//   const handleLogin = (isLogged) => {
+//     console.log(isLoggedIn);
+//     setIsLoggedIn(isLogged);
+//     console.log(isLoggedIn);
+//   };
+
+//   return (
+//     <BrowserRouter>
+//       <div className='app-container'>
+//         {isLoggedIn && <Sidebar />}
+//         <div className='routess'>
+//           <Routes>
+//             {isLoggedIn ? <Route path='/' element={<Home />} /> : <Route path='/' element={<Login onLogin={handleLogin} />} />}
+//             <Route path='/search' element={<Search />} />
+//             <Route path='/messages' element={<Messages />} />
+//             <Route path='/notifications' element={<Notifications />} />
+//             <Route path='/create' element={<Create />} />
+//             <Route path='/profile' element={<Profile />} />
+//             <Route path='/editprofile' element={<EditProfile />} />
+//             <Route path='/signup' element={<Signup />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
 import React, { useState } from 'react';
 import "./App.css";
 import Home from "./Pages/Home/Home";
-import Sidebar from "./Components/Sidebar/Sidebar";
 import Messages from "./Pages/Messages/Messages";
 import Search from "./Pages/Search/Search";
 import Create from "./Pages/Create/Create";
+import Sidebar from "./Components/Sidebar/Sidebar";
 import Notifications from "./Pages/Notifications/Notifications";
 import Profile from "./Pages/Profile/Profile";
 import Login from "./Pages/Login/Login";
@@ -61,31 +108,45 @@ import Signup from "./Pages/Signup/Signup";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import EditProfile from './Pages/EditProfile/EditProfile';
 
+function AuthenticatedRoutes() {
+  return (
+    <>
+      <Sidebar />
+      <div className='routess'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/messages' element={<Messages />} />
+          <Route path='/notifications' element={<Notifications />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/editprofile' element={<EditProfile />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+function UnauthenticatedRoutes({ handleLogin }) {
+  return (
+    <Routes>
+      <Route path='/' element={<Login onLogin={handleLogin} />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  );
+}
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const handleLogin = (isLogged) => {
-    console.log(isLoggedIn);
     setIsLoggedIn(isLogged);
-    console.log(isLoggedIn);
   };
 
   return (
     <BrowserRouter>
       <div className='app-container'>
-        {isLoggedIn && <Sidebar />}
-        <div className='routess'>
-          <Routes>
-            {isLoggedIn ? <Route path='/' element={<Home />} /> : <Route path='/' element={<Login onLogin={handleLogin} />} />}
-            <Route path='/search' element={<Search />} />
-            <Route path='/messages' element={<Messages />} />
-            <Route path='/notifications' element={<Notifications />} />
-            <Route path='/create' element={<Create />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/editprofile' element={<EditProfile />} />
-            <Route path='/signup' element={<Signup />} />
-          </Routes>
-        </div>
+        {isLoggedIn ? <AuthenticatedRoutes /> : <UnauthenticatedRoutes handleLogin={handleLogin} />} {/* Pass handleLogin to UnauthenticatedRoutes */}
       </div>
     </BrowserRouter>
   );
